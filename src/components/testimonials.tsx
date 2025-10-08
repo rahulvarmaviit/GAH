b7acf7bbe04f735efe8d098c6bb796b03111bc31
Joseph Ch <josepch.1057@gmail.com>
2025-10-08 09:45:03 +0000

    use the testimonials background as white

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -84,13 +84,13 @@ export const TestimonialsColumn = (props: {
           ease: "linear",
           repeatType: "loop",
         }}
-        className="flex flex-col gap-6 pb-6 bg-background"
+        className="flex flex-col gap-6 pb-6"
       >
         {[
           ...new Array(2).fill(0).map((_, index) => (
             <React.Fragment key={index}>
               {props.testimonials.map(({ text, image, name, role }, i) => (
-                <div className="p-10 rounded-3xl border shadow-lg shadow-primary/10 max-w-xs w-full" key={i}>
+                <div className="p-10 rounded-3xl border bg-card text-card-foreground shadow-lg shadow-primary/10 max-w-xs w-full" key={i}>
                   <div>{text}</div>
                   <div className="flex items-center gap-2 mt-5">
                     {image && (
@@ -105,7 +105,7 @@ export const TestimonialsColumn = (props: {
                     )}
                     <div className="flex flex-col">
                       <div className="font-medium tracking-tight leading-5">{name}</div>
-                      <div className="leading-5 opacity-60 tracking-tight">{role}</div>
+                      <div className="leading-5 text-muted-foreground tracking-tight">{role}</div>
                     </div>
                   </div>
                 </div>
@@ -120,7 +120,7 @@ export const TestimonialsColumn = (props: {
 
 export function Testimonials() {
   return (
-    <section className="bg-background my-20 relative">
+    <section className="bg-white dark:bg-slate-900/50 my-20 relative py-20">
 
       <div className="container z-10 mx-auto">
         <motion.div
