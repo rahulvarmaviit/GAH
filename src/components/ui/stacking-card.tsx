103b2afe264d395bf6c8d4bc4110491e7586f777
Joseph Ch <josepch.1057@gmail.com>
2025-10-09 16:15:23 +0000

    Also add a left to right gradient to it

--- src/components/ui/stacking-card.tsx
+++ src/components/ui/stacking-card.tsx
@@ -115,7 +115,7 @@ const Component = forwardRef<HTMLElement, ComponentRootProps>(({ projects }, ref
           <section className='text-white h-[70vh] w-full bg-slate-950 grid place-content-center'>
             <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
 
-            <h1 className='text-6xl sm:text-8xl md:text-9xl px-8 font-semibold text-center tracking-tight leading-[120%] text-primary'>
+            <h1 className='text-6xl sm:text-8xl md:text-9xl px-8 font-semibold text-center tracking-tight leading-[120%] bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent'>
               Our Products
             </h1>
           </section>
