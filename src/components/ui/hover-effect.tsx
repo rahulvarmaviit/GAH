4d65517785d92277fda05bb15ce4e0e717fdb3b3
Joseph Ch <josepch.1057@gmail.com>
2025-10-09 16:32:02 +0000

    change the color of hover to purple

--- src/components/ui/hover-effect.tsx
+++ src/components/ui/hover-effect.tsx
@@ -35,7 +35,7 @@ export const HoverEffect = ({
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-muted dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-primary/20 dark:bg-slate-800/[0.8] block  rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
