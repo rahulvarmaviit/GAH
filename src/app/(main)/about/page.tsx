2c79edfb2c62c59d9f5584e4871387badb5ddb7d
Joseph Ch <josepch.1057@gmail.com>
2025-12-03 11:36:12 +0000

    We are Global Acknowledgment Hub (_for element h1_)

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -59,7 +59,7 @@ export default function AboutPage() {
       <header className="h-[80vh] flex items-center justify-center text-center bg-black">
         <div className="container">
           <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-purple-600 to-purple-800 animate-gradient">
-            We are Acknowledgement Hub
+            We are Global Acknowledgment Hub
           </h1>
           <p className="mx-auto max-w-[700px] text-slate-300/80 md:text-xl mt-4">
             Innovators, dreamers, and builders dedicated to pushing the boundaries of what&apos;s possible.
