c0ef44dc64cb0d435f6cb2d6a609c45dccd5a34a
Rahul Varma <rahulvarmaviit@gmail.com>
2025-12-09 09:09:16 +0000

    add the background grid in products page to arcFeatureReveal section too

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -21,13 +21,14 @@ const timelineData = products.map(p => ({
 export default function ProductsPage() {
   return (
     <>
-      <section className='text-white h-auto py-48 w-full bg-slate-950 grid place-content-center'>
-        <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
+      <section className='text-white h-auto py-48 w-full bg-slate-950 grid place-content-center relative'>
+        <div className='absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
         <h1 className='text-6xl sm:text-8xl md:text-9xl px-8 font-semibold text-center tracking-tight leading-[120%] bg-gradient-to-r from-purple-400 via-primary to-purple-900 bg-clip-text text-transparent'>
           Our Products
         </h1>
       </section>
-      <section>
+      <section className="relative bg-slate-950">
+        <div className='absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
         <ArcFeatureReveal />
       </section>
       <section>
